cef17ec401725eaef505ba7280ba82bc0d5b77e6
Yashwanth Veerepalli <39512437+yashwanthveerepalli@users.noreply.github.com>
2023-01-29 23:03:54 -0600

    Add files via upload

---
+++ IntOptions.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class IntOptions {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<String>();
+        int sum =0,i=0,count=0;
+        String value ="";
+
+        Scanner s = new Scanner(System.in);
+        stringList.add("SOME 1");
+        stringList.add("NONE");
+        stringList.add("SOME 3");
+
+        System.out.println("Given Input "+stringList);
+
+
+        for(String string: stringList){
+            String expression = string.substring(0,4).toLowerCase(Locale.ROOT);
+            if(!expression.equals("none")) {
+                count = Integer.parseInt(string.substring(5));
+            }
+            switch(expression){
+                //Case statements
+                case "some":
+                    sum = count+sum;
+                    break;
+                case "none":
+                    break;
+                //Default case statement
+                default:System.out.println("Invalid Input");
+            }
+
+        }
+        if(sum > 0){
+            System.out.println("SOME "+sum);
+        }
+        else{
+            System.out.println("NONE");
+        }
+
+    }
+}
+
---
+++ MapString.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.*;
+
+public class MapString {
+
+    public static void main(String[] args) {
+
+
+        HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+
+        Scanner scan = new Scanner(System.in);
+        int count = 0;
+
+        System.out.println("Enter Size");
+        int size = scan.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter Integer and String");
+            hashmap.put(scan.nextInt(), scan.next());
+        }
+
+        System.out.println("Enter search string");
+        String string = scan.next();
+
+        for (Object key = hashmap.keySet()) {
+            Object value = hashmap.get(key);
+            if (value == string) {
+                Object name = key.toString();
+                System.out.println("SOME " + name);
+                count = 1;
+
+            }
+
+
+        }
+
+        if (count == 0) {
+            System.out.println("NONE");
+
+        }
+
+
+    }
+
+
+}
