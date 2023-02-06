4addc9bb2ab34940890d900834b0e49ff924a3c1
YashwanthVeerepalli <yashwanthveerepalli@gmail.com>
2023-02-05 20:52:11 -0600

    Moved to Assignment 2 Folder

--- src/assignment2/Misgrading.java
+++ src/assignment2/Misgrading.java
@@ -1,43 +1,37 @@
 package src.assignment2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Misgrading {
+    public static void misgrading(HashMap<String,Integer> grades){
+
+        int count =0;
+
+        for(Object key : grades.keySet()){
+            if(key == "pass" && 75 <=grades.get(key)){
+                count = count;
+            }
+            else if(key == "fail" && 75 > grades.get(key)){
+                count = count ;
+            }
+            else {
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+    }
 
     public static void main(String[] args) {
+        HashMap<String,Integer> grades = new HashMap<>();
 
 
-        HashMap<String,HashMap<Integer,Integer>> input = new HashMap<>();
+        grades.put("fail",90);
+        grades.put("pass",22);
 
-        HashMap<Integer, Integer> val = new HashMap<Integer, Integer>();
-
-
-        System.out.println("Enter Number of students in the class");
-        Scanner scanner = new Scanner(System.in);
-        int classStrength = scanner.nextInt();
-
-        for(int i=1;i<=classStrength;i++){
-            System.out.println("Enter Grades of the Student With ID"+i);
-
-            val.put(i,scanner.nextInt());
-
-            System.out.println("Enter The grade for the student with grade"+val.get(i));
-
-            input.put(scanner.next(),val);
-
-        }
-
-        System.out.println(input);
-
-        for(String grade : input.keySet()){
-            if(grade == "pass"){
-                HashMap<Integer, Integer> value = input.get(grade);
-                for(int result : value.keySet()){
-
-                }
-
-            }
-        }
+        misgrading(grades);
 
 
     }
